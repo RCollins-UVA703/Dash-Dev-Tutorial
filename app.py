3027fb88cf15136e32579ae5d1dc8d8f5bbf7607
Robert Parsons <bparsons@apogeeintegration.com>
2021-02-01 21:06:08 -0500

    Dark Layout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
@@ -13,6 +13,11 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
@@ -23,18 +28,31 @@ df = pd.DataFrame({
 
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure=fig
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
